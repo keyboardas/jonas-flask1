5155b638d6b0d321dbff68f60087fa94e519104c
Jonas Ikasala <jonas.ikasala@novusit.lt>
2024-05-14 21:41:43 +0300

    even more fix to the test

--- test/test_app.py
+++ test/test_app.py
@@ -7,26 +7,26 @@ from main import app
 
 
 @pytest.fixture
-def test_client():
+def client():
     """
     Fixture to set up a test client
     """
     app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
+    with app.test_client() as test_client:
+        yield test_client
 
 
-def test_index(test_client):
+def test_index(client):
     """
     Test the index route
     """
-    response = test_client.get('/')
+    response = client.get('/')
     assert b'<h1><center>This header was changed in a new_branch</center></h1>' in response.data
 
 
-def test_nonexistent_route(test_client):
+def test_nonexistent_route(client):
     """
     Test a nonexistent route
     """
-    response = test_client.get('/nonexistent')
+    response = client.get('/nonexistent')
     assert response.status_code == 404
