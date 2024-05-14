ae2597c91cb77da1e95482d0efd67b92930890a5
Jonas Ikasala <jonas.ikasala@novusit.lt>
2024-05-14 21:36:52 +0300

    docstrings added in the test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -1,19 +1,32 @@
+"""
+Module to test the Flask app
+"""
+
 import pytest
 from main import app
 
 
 @pytest.fixture
 def client():
+    """
+    Fixture to set up a test client
+    """
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
 
 def test_index(client):
+    """
+    Test the index route
+    """
     response = client.get('/')
     assert b'<h1><center>This header was changed in a new_branch</center></h1>' in response.data
 
 
 def test_nonexistent_route(client):
+    """
+    Test a nonexistent route
+    """
     response = client.get('/nonexistent')
     assert response.status_code == 404
